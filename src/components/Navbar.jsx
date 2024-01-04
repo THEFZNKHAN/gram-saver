6ce2dfad820f9a4e0c470b043f5c65f483b3e14f
Md Faizan Khan <FKHAN20040@GMAIL.COM>
2024-01-05 00:04:36 +0530

    Update pages

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { ReactComponent as Hamburger } from "../assets/hamburger.svg";
 
 import "./navbar.css";
 
 const Navbar = () => {
+  const [showNavbar, setShowNavbar] = useState(false);
+
+  const handleShowNavbar = () => {
+    setShowNavbar(!showNavbar);
+  };
+  
   return (
     <nav className="navbar">
-      <h1>Insta Saver</h1>
-      <p>Download Instagram Reels by pasting their URL in the input box.</p>
+      <div className="container">
+        <div className="logo">
+          <h1>Gram Saver</h1>
+        </div>
+        <div className="menu-icon" onClick={handleShowNavbar}>
+          <Hamburger />
+        </div>
+        <div className={`nav-elements  ${showNavbar && "active"}`}>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/reels">Reels</Link>
+            </li>
+            <li>
+              <Link to="/stories">Stories</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 };
